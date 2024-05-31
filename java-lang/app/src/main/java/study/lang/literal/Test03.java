e0360ddf5b53453aaa90fe94d6646be50a6b7ace
stereokim123 <rarangoon@gmail.com>
2024-05-31 18:17:18 +0900

    Java practice

--- java-lang/app/src/main/java/study/lang/literal/Test03.java
+++ java-lang/app/src/main/java/study/lang/literal/Test03.java
@@ -1,11 +1,12 @@
 package study.lang.literal;
 
-public class Test02 {
+public class Test03 {
     public static void main(String[] args) {
-        System.out.println(100);        // 10진수
-        System.out.println(0b1100100);  // 2진수
-        System.out.println(0144);       // 8진수
-        System.out.println(0x64);       // 16진수
+        System.out.println(3.14);        // 10진수
+        System.out.println();  // 31.4 * 
+        System.out.println();       // 8진수
+        System.out.println();       // 16진수
+        System.out.println();       // 16진수
     }    
 }
 
--- java-lang/app/src/main/java/study/lang/literal/Test05.java
+++ java-lang/app/src/main/java/study/lang/literal/Test05.java
@@ -5,7 +5,7 @@ package study.lang.literal;
 public class Test05  {
     public static void main(String[] args) {
         System.out.println("HelloWorld"); // 문자 리터럴
-        System.out.println("Hello\r\nWorld!"); // 문자 리터럴
+        System.out.println("Hello\r\nWorld!"); //   문자 리터럴
     }    
 }
 
