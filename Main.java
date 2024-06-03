b62c8e8ef7bc2a8628e5492fe06ab9935674cbb7
stereokim123 <rarangoon@gmail.com>
2024-06-03 20:32:13 +0900

    Unicode practice

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+        public sta
+        
+
+            int A = in.nextI n t()
+
+        
+        System.out.println(A - B);
+
+        in.close();
+    
+    }
+}
---
+++ java-lang/app/src/main/java/study/lang/literal/Test07.java
@@ -0,0 +1,10 @@
+// 실습
+// 12.375f 부동 소수점을 출력하라
+// 컴파일된 클래스 파일을 hexa로 읽어서 12.375가 2진수로 저장된 것을 확인하라
+
+public class Test07  {
+    public static void main(String[] args) {
+        System.out.println(12.375f); // 부동소수점이 메모리에 저장될 때 2진수로 바뀌는 것을 확인한다
+    }
+}
+
